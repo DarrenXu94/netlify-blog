17209770b500919b7ca1d74987b485a24f7b5c3d
Darren Xu <darrenxu1994@gmail.com>
2018-09-27 22:18:32 +1000

    variable anme change

--- src/config/contently.js
+++ src/config/contently.js
@@ -3,10 +3,11 @@ let devConfig
 if (process.env.NODE_ENV === "development"){
     devConfig = require('./devContently').default
 }
+console.log(process.env)
 
 const contentlyConfig = {
-    space: process.env.SPACE || devConfig.space,
-    accessToken: process.env.TOKEN || devConfig.accessToken
+    space: process.env.REACT_APP_SPACE || devConfig.space,
+    accessToken: process.env.REACT_APP_TOKEN || devConfig.accessToken
 }
 
 export default contentlyConfig
