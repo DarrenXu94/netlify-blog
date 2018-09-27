7aa20bb51d07c93db3c37ee6554e510646752c04
Darren Xu <darrenxu1994@gmail.com>
2018-09-27 22:03:07 +1000

    local working

--- src/config/contently.js
+++ src/config/contently.js
@@ -1,6 +1,12 @@
-console.log(process)
-const contentlyConfig = {
-    space: process.env.SPACE || 'space',
-    accessToken: process.env.TOKEN || 'token'
+let devConfig
+
+if (process.env.NODE_ENV === "development"){
+    devConfig = require('./devContently').default
 }
+
+const contentlyConfig = {
+    space: process.env.SPACE || devConfig.space,
+    accessToken: process.env.TOKEN || devConfig.accessToken
+}
+
 export default contentlyConfig
