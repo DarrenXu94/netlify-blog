8af07fdc053b5d47f39c1143c838a1f248ed49be
Darren Xu <darrenxu1994@gmail.com>
2018-09-27 22:20:46 +1000

    remove toml

--- src/config/contently.js
+++ src/config/contently.js
@@ -3,7 +3,6 @@ let devConfig
 if (process.env.NODE_ENV === "development"){
     devConfig = require('./devContently').default
 }
-console.log(process.env)
 
 const contentlyConfig = {
     space: process.env.REACT_APP_SPACE || devConfig.space,
