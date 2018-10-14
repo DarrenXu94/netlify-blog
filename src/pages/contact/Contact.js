74049e68d680c680472a145ae13ea87d7a9c3e85
Darren Xu <darrenxu1994@gmail.com>
2018-10-14 16:51:23 +1100

    font awesome

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faMailchimp } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
 
 const Contact = () => {
     return (
         <div>
             <h3>Catch me on the following platforms</h3>
+            <div className="flex" style={{justifyContent: 'space-evenly'}}>
+            <Button className="margin5" bsStyle="primary" bsSize="large">
+            <FontAwesomeIcon icon={faGithub} />
+            <b className="margin10">Github</b>
+            </Button>
+            <Button className="margin5" bsStyle="primary" bsSize="large">
+            <FontAwesomeIcon icon={faLinkedin} />
+            <b className="margin10">LinkedIn</b>
 
-            <Button className="margin5" bsStyle="primary" bsSize="large">Github</Button>
-            <Button className="margin5" bsStyle="primary" bsSize="large">LinkedIn</Button>
-            <Button className="margin5" bsStyle="primary" bsSize="large">Email</Button>
+            </Button>
+            <Button className="margin5" bsStyle="primary" bsSize="large">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <b className="margin10">Email</b>
+            </Button>
+            
+            </div>
 
         </div>
     );
