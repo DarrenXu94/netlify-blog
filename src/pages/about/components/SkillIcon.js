d601ea9a35357e05f695b8728186bbd634c6cc3a
Darren <darrenxu1994@gmail.com>
2018-10-24 21:16:03 +1100

    svg handling

--- src/pages/about/components/SkillIcon.js
+++ src/pages/about/components/SkillIcon.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const SkillIcon = ({item}) => {
+const color = '#3f464d'
+const dimensions = { width: '196', height: '196' }
+
+const SkillIcon = ({ item }) => {
+    const path = item.raw.split(`<path style=" " d="`)[1]
+    const raw_path = path.substring(0, path.lastIndexOf(`"`))
+
     return (
-        <div style={{textAlign:"center"}}>
-            <img src={item.src} alt=""></img>
-            <h3>{item.name}</h3>
+        <div style={{ textAlign: "center" }}>
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                width={dimensions.width} height={dimensions.height}
+                viewBox="0 0 50 50"
+                style={{fill:`${color}`}}><g id={item.name}><path d={`${raw_path}`}></path></g></svg>
+                <h3 style={{color:`${color}`}}>{item.name}</h3>
 
         </div>
     );
