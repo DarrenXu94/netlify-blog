3a641d0819da8a0023958bdf2f3b630ba338e2a9
Darren <darrenxu1994@gmail.com>
2018-10-27 14:42:07 +1100

    contact goes places

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -4,26 +4,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
+const GitHub = `https://github.com/DarrenXu94`
+const LinkedIn = `https://www.linkedin.com/in/darren-xu-profile/`
 
 const Contact = () => {
     return (
         <div>
             <h3>Catch me on the following platforms</h3>
-            <div className="flex" style={{justifyContent: 'space-evenly'}}>
-            <Button className="margin5" bsStyle="primary" bsSize="large">
-            <FontAwesomeIcon icon={faGithub} />
-            <b className="margin10">Github</b>
-            </Button>
-            <Button className="margin5" bsStyle="primary" bsSize="large">
-            <FontAwesomeIcon icon={faLinkedin} />
-            <b className="margin10">LinkedIn</b>
+            <div className="flex" style={{ justifyContent: 'space-evenly' }}>
+                <Button href={GitHub} className="margin5" bsStyle="primary" bsSize="large">
+                    <FontAwesomeIcon icon={faGithub} />
+                    <b className="margin10">Github</b>
+                </Button>
+                <Button href={LinkedIn} className="margin5" bsStyle="primary" bsSize="large">
+                    <FontAwesomeIcon icon={faLinkedin} />
+                    <b className="margin10">LinkedIn</b>
 
-            </Button>
-            <Button className="margin5" bsStyle="primary" bsSize="large">
-            <FontAwesomeIcon icon={faEnvelope} />
-            <b className="margin10">Email</b>
-            </Button>
-            
+                </Button>
+                <Button className="margin5" bsStyle="primary" bsSize="large">
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    <b className="margin10">Email</b>
+                </Button>
+
             </div>
 
         </div>
