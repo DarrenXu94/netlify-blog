be106283da9124e0646c53ce95a2b9e09c528b06
Darren <darrenxu1994@gmail.com>
2018-10-28 11:02:56 +1100

    copy to clipboard

--- src/pages/contact/EmailButton.js
+++ src/pages/contact/EmailButton.js
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 class EmailButton extends Component {
     state = {
-        showEmail: false
+        showEmail: false,
+        copied: false
     }
     render() {
-        let { showEmail } = this.state;
+        let { showEmail, copied } = this.state;
         return (
-            <Button onClick={()=>this.setState({showEmail: !showEmail})} className="margin5" bsStyle="primary" bsSize="large">
-                <FontAwesomeIcon icon={faEnvelope} />
-                <b className="margin10">{showEmail ? "darrenxu1994@gmail.com" : "Email"}</b>
-            </Button>
+            <div style={{display:"flex", flexDirection: "column"}}>
+
+                <Button onClick={() => this.setState({ showEmail: !showEmail })} className="margin5" bsStyle="primary" bsSize="large">
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    <b className="margin10">{showEmail ? "darrenxu1994@gmail.com" : "Email"}</b>
+                </Button>
+                {showEmail &&
+                    <CopyToClipboard text={`darrenxu1994@gmail.com`}
+                        onCopy={() => this.setState({ copied: true })}>
+                        <Button>{copied ? "Copied" : "Copy to clipboard"}</Button>
+                    </CopyToClipboard>
+                }
+                
+
+            </div>
         );
     }
 }
